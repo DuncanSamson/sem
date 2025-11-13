29202ffcfd963aebd3e06b2e8129b508818aa9ea
DuncanSamson <15039074+DuncanSamson@users.noreply.github.com>
2025-11-13 17:11:38 +0000

    #33 Add TtheTopNPopulatedCountriesInAContinentWhereNIsProvidedByTheUser report with test

--- src/test/java/com/napier/sem/WorldIT.java
+++ src/test/java/com/napier/sem/WorldIT.java
@@ -151,4 +151,16 @@ public class WorldIT
 
     }
 
+    @Test
+        void testTheTopNPopulatedCountriesInAContinentWhereNIsProvidedByTheUser() {
+        List<Country> countries = reports.theTopNPopulatedCountriesInAContinentWhereNIsProvidedByTheUser(5);
+        assertEquals(5, countries.size(), "The total number of countries should be 5.");
+
+        var firstCountry = countries.get(0);
+        var firstPopulation = firstCountry.getPopulation();
+        var secondCountry = countries.get(1);
+        var secondPopulation = secondCountry.getPopulation();
+        assertTrue(firstPopulation > secondPopulation, "The population should be greater than the first population.");
+    }
+
 }
