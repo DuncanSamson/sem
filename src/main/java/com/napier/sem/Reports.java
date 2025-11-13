46e025ed39413ef0cc430556948f2f22f40059db
DuncanSamson <15039074+DuncanSamson@users.noreply.github.com>
2025-11-13 23:37:16 +0000

    #36 Add testAllTheCitiesInAContinentWhereNIsProvidedByTheUser report with test

--- src/main/java/com/napier/sem/Reports.java
+++ src/main/java/com/napier/sem/Reports.java
@@ -113,6 +113,26 @@ public class Reports {
         return countries;
     }
 
+    public List<City> AllTheCitiesInAContinentOrganisedByLargestPopulationToSmallest(String continentName) {
+        String query = "SELECT ci.ID, ci.Name, ci.CountryCode, ci.Population, ci.District FROM country c JOIN city ci ON c.Code = ci.CountryCode WHERE c.Continent LIKE ? ORDER BY c.Population DESC";
+        List<City> cities = new ArrayList<>();
+        System.out.println("AllTheCitiesInAContinentWhereNIsProvidedByTheUser: " + query);
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, continentName);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    City c = City.fromResultSet(resultSet);
+                    cities.add(c);
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+
+
 }
 
 
