30a99445bd070abcb182fe4d0004b842a98d3ced
Plinio Hernandez Martos <pliniohernandes@gmail.com>
2025-11-28 12:10:03 +0000

    test: Add CountryTest to run more tests

---
+++ src/test/java/com/napier/sem/CountryTest.java
@@ -0,0 +1,11 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+    @Test
+    void test() {
+        assertEquals(5, 5);
+    }
+}
